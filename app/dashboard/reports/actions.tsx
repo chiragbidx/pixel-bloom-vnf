f06c733391bcf96d00e6f6926361d76911d31132
chiragbidx <chirag@bidx.ai>
2026-03-17 13:53:14 +0530

    Reports Page Actions

---
+++ app/dashboard/reports/actions.tsx
@@ -0,0 +1,7 @@
+"use server";
+
+// For production, provide server-side logic to generate/download reports
+export async function getReports(userId: string) {
+  // Generate or fetch report metadata.
+  return [];
+}
